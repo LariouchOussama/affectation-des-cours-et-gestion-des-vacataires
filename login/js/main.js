c8a6866053b06edea9b463f03acf69e5574d02a1
Lariouch Oussama <90968210+LariouchOussama@users.noreply.github.com>
2023-06-23 11:30:31 +0100

    Add files via upload

---
+++ login/js/main.js
@@ -0,0 +1,23 @@
+// Get the search input and search button elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+// Get the table body element
+const tableBody = document.querySelector('#vacataire-table tbody');
+
+// Add event listener to the search button
+searchButton.addEventListener('click', () => {
+    const searchText = searchInput.value.toLowerCase();
+
+    // Loop through each table row and hide/show based on the search text
+    for (const row of tableBody.rows) {
+        const firstName = row.cells[0].textContent.toLowerCase();
+        const lastName = row.cells[1].textContent.toLowerCase();
+
+        if (firstName.includes(searchText) || lastName.includes(searchText)) {
+            row.style.display = ''; // Show the row
+        } else {
+            row.style.display = 'none'; // Hide the row
+        }
+    }
+});
